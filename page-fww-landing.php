ddf2b44d48fddb03c9f288a180f5adb15e1241f1
Chris Bishop <domingobishop@gmail.com>
2016-03-08 16:25:40 +0000

    added loop to page template

--- page-fww-landing.php
+++ page-fww-landing.php
@@ -9,11 +9,16 @@ get_header(); ?>
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
-					<?php
-					while ( have_posts() ) : the_post();
-						get_template_part( 'content' );
-					endwhile;
-					?>
+					<?php while ( have_posts() ) : the_post(); ?>
+						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+							<div class="entry-header">
+								<h1><?php the_title(); ?></h1>
+							</div>
+							<div class="entry-content">
+								<?php the_content(); ?>
+							</div>
+						</article>
+					<?php endwhile; ?>
 				</div>
 			</div>
 		</div>
