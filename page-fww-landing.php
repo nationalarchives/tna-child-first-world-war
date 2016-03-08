549d5d399ba0b1269553518294cbc4af3dba8315
Chris Bishop <domingobishop@gmail.com>
2016-03-08 16:23:06 +0000

    removed sidebar

--- page-fww-landing.php
+++ page-fww-landing.php
@@ -5,26 +5,18 @@ Template Name: FWW landing
 get_header(); ?>
 <?php get_template_part( 'breadcrumb' ); ?>
 
-	<div id="primary" class="content-area">
+	<main id="main" class="content-area" role="main">
 		<div class="container">
 			<div class="row">
-				<main id="main" class="col-xs-12 col-sm-8 col-md-8" role="main">
+				<div class="col-md-12">
 					<?php
 					while ( have_posts() ) : the_post();
 						get_template_part( 'content' );
 					endwhile;
 					?>
-				</main>
-				<?php
-				$sidebar = get_post_meta( $post->ID, 'sidebar', true );
-				if ( $sidebar == 'false' ) {
-					// do nothing
-				} else {
-					get_sidebar( $sidebar );
-				}
-				?>
+				</div>
 			</div>
 		</div>
-	</div>
+	</main>
 
 <?php get_footer(); ?>
