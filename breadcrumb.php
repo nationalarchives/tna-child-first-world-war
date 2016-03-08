2895ef7d937b1415c5ab4fb0e98b16a0ec627be7
Chris Bishop <domingobishop@gmail.com>
2016-03-08 16:31:30 +0000

    added breadcrumbs

---
+++ breadcrumb.php
@@ -0,0 +1,9 @@
+<div id="breadcrumb-holder" class="tna-breadcrumb">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
+			</div>
+		</div>
+	</div>
+</div>
