951b6126900f7d0129b37c5662b3df2f1c8afacd
Chris Bishop <domingobishop@gmail.com>
2016-03-09 12:08:46 +0000

    removed post meta functions

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 function dequeue_parent_style() {
     wp_dequeue_style('tna-styles');
     wp_deregister_style('tna-styles');
@@ -17,16 +19,4 @@ function tna_child_styles() {
 add_action( 'wp_enqueue_scripts', 'tna_child_styles' );
 
 
-// REMOVE POST META BOXES
-if (!function_exists('remove_page_metaboxes')) {
-    function remove_page_metaboxes() {
-        remove_meta_box('commentstatusdiv', 'page', 'normal'); // Comments Metabox
-        remove_meta_box('trackbacksdiv', 'page', 'normal'); // Talkback Metabox
-        remove_meta_box('slugdiv', 'page', 'normal'); // Slug Metabox
-        remove_meta_box('authordiv', 'page', 'normal'); // Author Metabox
-        remove_meta_box('postimagediv', 'page', 'normal'); // Featured Image Metabox
-        remove_meta_box('postcustom', 'page', 'normal');
-        remove_meta_box('commentsdiv', 'page', 'normal');
-    }
-}
-add_action('admin_menu','remove_page_metaboxes');
+
