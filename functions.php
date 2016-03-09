7df478dcdc5861724e5f4b82a23b04958a76a647
Chris Bishop <domingobishop@gmail.com>
2016-03-09 12:22:46 +0000

    theme globals added

--- functions.php
+++ functions.php
@@ -1,6 +1,24 @@
 <?php
 
-
+// Edit as required
+function tnatheme_globals() {
+    global $pre_path;
+    global $pre_crumbs;
+    if (substr($_SERVER['REMOTE_ADDR'], 0, 3) === '10.') {
+        $pre_path = '';
+        $pre_crumbs = array(
+            'Commercial opportunities' => '/'
+        );
+    } else {
+        $pre_crumbs = array(
+            'About us' => '/about/',
+            'Commercial opportunities' => '/about/commercial-opportunities/'
+        );
+        $pre_path = '/about/commercial-opportunities';
+    }
+}
+// For live environment
+// tnatheme_globals();
 
 function dequeue_parent_style() {
     wp_dequeue_style('tna-styles');
