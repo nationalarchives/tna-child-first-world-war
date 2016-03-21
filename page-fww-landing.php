55a538a445b246b132e5135529bf691cdcdcbba1
Chris Bishop <domingobishop@gmail.com>
2016-03-21 16:51:38 +0000

    banner image

--- page-fww-landing.php
+++ page-fww-landing.php
@@ -7,7 +7,23 @@ get_header(); ?>
 	<div class="banner" role="banner">
 		<?php get_template_part( 'breadcrumb' ); ?>
 		<div class="heading-banner text-center">
-			<h1>First World War</h1>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<img src="<?php echo get_stylesheet_directory_uri() ?>/img/fww-logo.png">
+						<h1>First World War</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div role="navigation">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					Menu bar
+				</div>
+			</div>
 		</div>
 	</div>
 	<main id="main" class="content-area" role="main">
