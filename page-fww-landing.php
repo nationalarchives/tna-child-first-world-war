e86fb4736ef95cb17bb04fe6dbc1efdcd5598af5
Chris Bishop <domingobishop@gmail.com>
2016-03-22 16:02:10 +0000

    basic layout

--- page-fww-landing.php
+++ page-fww-landing.php
@@ -3,30 +3,30 @@
 Template Name: FWW landing
 */
 get_header(); ?>
-<div class="fww">
-	<div class="banner" role="banner">
-		<?php get_template_part( 'breadcrumb' ); ?>
-		<div class="heading-banner text-center">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12">
-						<img src="<?php echo get_stylesheet_directory_uri() ?>/img/fww-logo.png">
-						<h1>First World War</h1>
+	<div class="fww">
+		<div class="banner" role="banner">
+			<?php get_template_part( 'breadcrumb' ); ?>
+			<div class="heading-banner text-center">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<img src="<?php echo get_stylesheet_directory_uri() ?>/img/fww-logo.png">
+							<h1>First World War</h1>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-	<div class="fww-menu" role="navigation">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12 text-center">
-					Menu bar
+		<div class="fww-menu" role="navigation">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 text-center">
+						Menu bar
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-	<main id="main" class="content-area" role="main">
+		<main id="main" class="content-area" role="main">
 			<div class="container">
 				<section class="social">
 					<div class="row">
@@ -34,9 +34,11 @@ get_header(); ?>
 							<h2>Stay up-to-date with all our centenary activity</h2>
 						</div>
 						<div class="col-md-4">
-							<form name="signup" id="banner-form" class="pad-medium" action="http://dmtrk.co.uk/signup.ashx"
+							<form name="signup" id="banner-form" class="pad-medium"
+							      action="http://dmtrk.co.uk/signup.ashx"
 							      method="post">
-								<input type="hidden" name="addressbookid" value="281378"><input type="hidden" name="userid"
+								<input type="hidden" name="addressbookid" value="281378"><input type="hidden"
+								                                                                name="userid"
 								                                                                value="28895">
 								<input type="hidden" name="ReturnURL"
 								       value="http://nationalarchives.gov.uk/news/subscribe-confirmation.htm">
@@ -55,7 +57,7 @@ get_header(); ?>
 					<div class="row">
 						<div class="col-md-4">
 							<div class="fww-box clearfix">
-								<img src="http://placehold.it/360x160" class="img-responsive">
+								<img src="http://placehold.it/440x160" class="img-responsive">
 								<div class="entry-fww">
 									<p>Event</p>
 									<h2>Title</h2>
@@ -65,7 +67,7 @@ get_header(); ?>
 						</div>
 						<div class="col-md-4">
 							<div class="fww-box clearfix">
-								<img src="http://placehold.it/360x160" class="img-responsive">
+								<img src="http://placehold.it/440x160" class="img-responsive">
 								<div class="entry-fww">
 									<p>Feature</p>
 									<h2>Title</h2>
@@ -75,7 +77,37 @@ get_header(); ?>
 						</div>
 						<div class="col-md-4">
 							<div class="fww-box clearfix">
-								<img src="http://placehold.it/360x160" class="img-responsive">
+								<img src="http://placehold.it/440x160" class="img-responsive">
+								<div class="entry-fww">
+									<p>Blog</p>
+									<h2>Title</h2>
+									<p>Eu congue salutatus philosophia per. Nec ex admodum gubergren.</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="fww-box clearfix">
+								<img src="http://placehold.it/440x160" class="img-responsive">
+								<div class="entry-fww">
+									<p>Event</p>
+									<h2>Title</h2>
+									<p>Eu congue salutatus philosophia per. Nec ex admodum gubergren.</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="fww-box clearfix">
+								<img src="http://placehold.it/440x160" class="img-responsive">
+								<div class="entry-fww">
+									<p>Feature</p>
+									<h2>Title</h2>
+									<p>Eu congue salutatus philosophia per. Nec ex admodum gubergren.</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="fww-box clearfix">
+								<img src="http://placehold.it/440x160" class="img-responsive">
 								<div class="entry-fww">
 									<p>Blog</p>
 									<h2>Title</h2>
@@ -85,21 +117,124 @@ get_header(); ?>
 						</div>
 					</div>
 				</section>
-			<div class="row">
-				<div class="col-md-12">
-					<?php while ( have_posts() ) : the_post(); ?>
-						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<div class="row">
+					<div class="col-md-12">
+						<article>
 							<div class="entry-header">
-								<h2><?php the_title(); ?></h2>
+								<h2>Explore our records</h2>
 							</div>
-							<div class="entry-content">
-								<?php the_content(); ?>
+							<div class="entry-content clearfix">
+								<div class="col-md-4">
+									<div class="fww-box clearfix">
+										<a href="#"><img src="http://placehold.it/360x160" class="img-responsive"></a>
+										<div class="entry-fww">
+											<h3><a href="#">Title</a></h3>
+											<p>Eu congue salutatus philosophia per. Nec ex admodum gubergren.</p>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="fww-box clearfix">
+										<a href="#"><img src="http://placehold.it/360x160" class="img-responsive"></a>
+										<div class="entry-fww">
+											<h3><a href="#">Title</a></h3>
+											<p>Eu congue salutatus philosophia per. Nec ex admodum gubergren.</p>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="fww-box clearfix">
+										<a href="#"><img src="http://placehold.it/360x160" class="img-responsive"></a>
+										<div class="entry-fww">
+											<h3><a href="#">Title</a></h3>
+											<p>Eu congue salutatus philosophia per. Nec ex admodum gubergren.</p>
+										</div>
+									</div>
+								</div>
 							</div>
 						</article>
-					<?php endwhile; ?>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<article>
+							<div class="entry-header">
+								<h2>Discover personal stories</h2>
+							</div>
+							<div class="entry-content clearfix">
+								<div class="col-md-6">
+									<div class="fww-box clearfix">
+										<a href="#"><img src="http://placehold.it/560x160" class="img-responsive"></a>
+										<div class="entry-fww">
+											<h3><a href="#">Title</a></h3>
+											<p>Eu congue salutatus philosophia per. Nec ex admodum gubergren.</p>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="fww-box clearfix">
+										<a href="#"><img src="http://placehold.it/560x160" class="img-responsive"></a>
+										<div class="entry-fww">
+											<h3><a href="#">Title</a></h3>
+											<p>Eu congue salutatus philosophia per. Nec ex admodum gubergren.</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</article>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-4">
+						<article>
+							<div class="entry-header">
+								<h2>About our programme</h2>
+							</div>
+							<div class="entry-content clearfix">
+									<div class="fww-box clearfix">
+										<a href="#"><img src="http://placehold.it/560x160" class="img-responsive"></a>
+										<div class="entry-fww">
+											<h3><a href="#">Title</a></h3>
+											<p>Eu congue salutatus philosophia per. Nec ex admodum gubergren.</p>
+										</div>
+									</div>
+							</div>
+						</article>
+					</div>
+					<div class="col-md-4">
+						<article>
+							<div class="entry-header">
+								<h2>What's on</h2>
+							</div>
+							<div class="entry-content clearfix">
+								<div class="fww-box clearfix">
+									<a href="#"><img src="http://placehold.it/560x160" class="img-responsive"></a>
+									<div class="entry-fww">
+										<h3><a href="#">Title</a></h3>
+										<p>Eu congue salutatus philosophia per. Nec ex admodum gubergren.</p>
+									</div>
+								</div>
+							</div>
+						</article>
+					</div>
+					<div class="col-md-4">
+						<article>
+							<div class="entry-header">
+								<h2>Bookshop</h2>
+							</div>
+							<div class="entry-content clearfix">
+								<div class="fww-box clearfix">
+									<a href="#"><img src="http://placehold.it/560x160" class="img-responsive"></a>
+									<div class="entry-fww">
+										<h3><a href="#">Title</a></h3>
+										<p>Eu congue salutatus philosophia per. Nec ex admodum gubergren.</p>
+									</div>
+								</div>
+							</div>
+						</article>
+					</div>
 				</div>
 			</div>
-		</div>
-	</main>
-</div>
+		</main>
+	</div>
 <?php get_footer(); ?>
