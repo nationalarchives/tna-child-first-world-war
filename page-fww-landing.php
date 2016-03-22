fe6066630413e357350fc34fa7929223cd5198b9
Chris Bishop <domingobishop@gmail.com>
2016-03-22 17:11:18 +0000

    content

--- page-fww-landing.php
+++ page-fww-landing.php
@@ -60,8 +60,8 @@ get_header(); ?>
 								<img src="http://placehold.it/440x160" class="img-responsive">
 								<div class="entry-fww">
 									<p>Event</p>
-									<h2>Title</h2>
-									<p>Eu congue salutatus philosophia per. Nec ex admodum gubergren.</p>
+									<h2><a href="#">Hands on History: Theatre in the First World War</a></h2>
+									<p>Friday 11 March, 09:45-16:30</p>
 								</div>
 							</div>
 						</div>
@@ -70,8 +70,9 @@ get_header(); ?>
 								<img src="http://placehold.it/440x160" class="img-responsive">
 								<div class="entry-fww">
 									<p>Feature</p>
-									<h2>Title</h2>
-									<p>Eu congue salutatus philosophia per. Nec ex admodum gubergren.</p>
+									<h2><a href="#">Unit war diaries</a></h2>
+									<p>We've published more digitised unit war diaries on Discovery.
+										Search now by regiment, battalion, brigade or division number:</p>
 								</div>
 							</div>
 						</div>
@@ -80,28 +81,20 @@ get_header(); ?>
 								<img src="http://placehold.it/440x160" class="img-responsive">
 								<div class="entry-fww">
 									<p>Blog</p>
-									<h2>Title</h2>
-									<p>Eu congue salutatus philosophia per. Nec ex admodum gubergren.</p>
+									<h2><a href="#">Building a virtual First World War village</a></h2>
+									<p>Within the archives there are many stories of events on the Home Front during the First World War.</p>
 								</div>
 							</div>
 						</div>
-						<div class="col-md-4">
-							<div class="fww-box clearfix">
-								<img src="http://placehold.it/440x160" class="img-responsive">
-								<div class="entry-fww">
-									<p>Event</p>
-									<h2>Title</h2>
-									<p>Eu congue salutatus philosophia per. Nec ex admodum gubergren.</p>
-								</div>
-							</div>
 						</div>
+					<div class="row">
 						<div class="col-md-4">
 							<div class="fww-box clearfix">
 								<img src="http://placehold.it/440x160" class="img-responsive">
 								<div class="entry-fww">
 									<p>Feature</p>
-									<h2>Title</h2>
-									<p>Eu congue salutatus philosophia per. Nec ex admodum gubergren.</p>
+									<h2><a href="#">Interactive global First World War map</a></h2>
+									<p>Explore the global impact of the First World War through our new online map, which highlights key events and figures in the conflict from our records.</p>
 								</div>
 							</div>
 						</div>
@@ -109,9 +102,19 @@ get_header(); ?>
 							<div class="fww-box clearfix">
 								<img src="http://placehold.it/440x160" class="img-responsive">
 								<div class="entry-fww">
-									<p>Blog</p>
-									<h2>Title</h2>
-									<p>Eu congue salutatus philosophia per. Nec ex admodum gubergren.</p>
+									<p>News</p>
+									<h2><a href="#">Works by artist Sarah Kogan to be shown at The National Archives</a></h2>
+									<p>We have today announced an exhibition of contemporary art, set to be welcomed to The National Archives next month. Changing the Landscape, an ambitious...</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="fww-box clearfix">
+								<img src="http://placehold.it/440x160" class="img-responsive">
+								<div class="entry-fww">
+									<p>News</p>
+									<h2><a href="#">First World War related baby names revealed</a></h2>
+									<p>There were 1,634 babies given First World War related names during the war (1914-1919) in England and Wales, new data analysis reveals. The battles...</p>
 								</div>
 							</div>
 						</div>
