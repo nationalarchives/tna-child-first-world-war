94f31b63eb7713c890ece15145d93ae1422b9d2f
Chris Bishop <domingobishop@gmail.com>
2016-04-04 16:14:17 +0100

    make paths relative

--- page-fww-landing.php
+++ page-fww-landing.php
@@ -10,7 +10,7 @@ get_header(); ?>
 				<div class="container">
 					<div class="row">
 						<div class="col-md-12">
-							<img src="<?php echo get_stylesheet_directory_uri() ?>/img/fww-logo.png" alt="First World War" class="img-responsive">
+							<img src="<?php echo make_path_relative( get_stylesheet_directory_uri() ) ?>/img/fww-logo.png" alt="First World War" class="img-responsive">
 							<h1>First World War</h1>
 						</div>
 					</div>
@@ -64,7 +64,7 @@ get_header(); ?>
 							<div id="event" class="card">
 								<div class="entry-thumbnail">
 									<a href="#about-our-programme">
-										<img src="<?php echo str_replace( home_url(), '', get_stylesheet_directory_uri() ) ?>/img/thumb-news.jpg" alt="First World War events">
+										<img src="<?php echo make_path_relative( get_stylesheet_directory_uri() ) ?>/img/thumb-news.jpg" alt="First World War events">
 									</a>
 								</div>
 								<div class="entry-content">
@@ -83,13 +83,13 @@ get_header(); ?>
 							<div class="col-sm-4">
 								<div class="card">
 									<div class="entry-thumbnail">
-										<a href="<?php the_permalink(); ?>">
-											<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+										<a href="<?php echo make_path_relative( get_page_link() ); ?>">
+											<?php echo make_path_relative( get_the_post_thumbnail( $post->ID, 'large' ) ); ?>
 										</a>
 									</div>
 									<div class="entry-content">
 										<small>Feature</small>
-										<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+										<h2><a href="<?php echo make_path_relative( get_page_link() ); ?>"><?php the_title(); ?></a></h2>
 										<?php the_excerpt(); ?>
 									</div>
 								</div>
@@ -98,8 +98,8 @@ get_header(); ?>
 						wp_reset_postdata();?>
 					</div>
 					<div class="row equal-heights">
-						<?php fww_rss( 'http://blog.nationalarchives.gov.uk/blog/tag/first-world-war/feed/', '1' ) ?>
-						<?php fww_news_rss( 'http://www.nationalarchives.gov.uk/category/first-world-war-portal-news/feed/', '2' ) ?>
+						<?php make_path_relative( fww_rss( 'http://blog.nationalarchives.gov.uk/blog/tag/first-world-war/feed/', '1' ) ) ?>
+						<?php make_path_relative( fww_news_rss( 'http://www.nationalarchives.gov.uk/category/first-world-war-portal-news/feed/', '2' ) ) ?>
 					</div>
 				</section>
 				<section id="explore-our-records">
@@ -114,7 +114,7 @@ get_header(); ?>
 										<div class="fww-box clearfix">
 											<div class="thumb-img">
 												<a href="#">
-													<img src="<?php echo get_stylesheet_directory_uri() ?>/img/fww-records.jpg" class="img-responsive" alt="Find your ancestor in our FWW records">
+													<img src="<?php echo make_path_relative( get_stylesheet_directory_uri() ) ?>/img/fww-records.jpg" class="img-responsive" alt="Find your ancestor in our FWW records">
 												</a>
 											</div>
 												<h3><a href="#">Find your ancestor in our FWW records</a></h3>
@@ -125,7 +125,7 @@ get_header(); ?>
 										<div class="fww-box clearfix">
 											<div class="thumb-img">
 												<a href="first-world-war/centenary-digitised-records/">
-													<img src="<?php echo get_stylesheet_directory_uri() ?>/img/online-collection.jpg" class="img-responsive" alt="Search our online collections">
+													<img src="<?php echo make_path_relative( get_stylesheet_directory_uri() ) ?>/img/online-collection.jpg" class="img-responsive" alt="Search our online collections">
 												</a>
 											</div>
 												<h3><a href="first-world-war/centenary-digitised-records/">Browse our online collections</a></h3>
@@ -152,7 +152,7 @@ get_header(); ?>
 										<div class="fww-box clearfix">
 											<div class="thumb-img">
 												<a href="http://www.operationwardiary.org/">
-													<img src="<?php echo get_stylesheet_directory_uri() ?>/img/unit-diaries.jpg" class="img-responsive" alt="Help us tag our war diaries">
+													<img src="<?php echo make_path_relative( get_stylesheet_directory_uri() ) ?>/img/unit-diaries.jpg" class="img-responsive" alt="Help us tag our war diaries">
 												</a>
 											</div>
 												<h3><a href="http://www.operationwardiary.org/">Help us tag our war diaries</a></h3>
@@ -177,7 +177,7 @@ get_header(); ?>
 										<div class="fww-box clearfix">
 											<div class="thumb-img">
 												<a href="http://www.nationalarchives.gov.uk/education/resources/letters-first-world-war-1915/">
-													<img src="<?php echo get_stylesheet_directory_uri() ?>/img/soldiers-MUN-5_157_1916-1918.jpg" class="img-responsive" alt="Letters from the First World War">
+													<img src="<?php echo make_path_relative( get_stylesheet_directory_uri() ) ?>/img/soldiers-MUN-5_157_1916-1918.jpg" class="img-responsive" alt="Letters from the First World War">
 												</a>
 											</div>
 												<small>Soldiers' letters</small>
@@ -189,7 +189,7 @@ get_header(); ?>
 										<div class="fww-box clearfix">
 											<div class="thumb-img">
 												<a href="http://media.nationalarchives.gov.uk/index.php/tag/first-world-war/">
-													<img src="<?php echo get_stylesheet_directory_uri() ?>/img/podcasts-MUN-5_394_40_June-1915.jpg" class="img-responsive" alt="First World War poscasts and videos">
+													<img src="<?php echo make_path_relative( get_stylesheet_directory_uri() ) ?>/img/podcasts-MUN-5_394_40_June-1915.jpg" class="img-responsive" alt="First World War poscasts and videos">
 												</a>
 											</div>
 												<small>Media</small>
@@ -212,7 +212,7 @@ get_header(); ?>
 								<div class="entry-content clearfix">
 									<div class="fww-box clearfix">
 										<div class="thumb-img">
-											<img src="<?php echo get_stylesheet_directory_uri() ?>/img/programme-MUN-5_165_1124_48_1_1920.jpg" class="img-responsive" alt="About our programme - First World War 100">
+											<img src="<?php echo make_path_relative( get_stylesheet_directory_uri() ) ?>/img/programme-MUN-5_165_1124_48_1_1920.jpg" class="img-responsive" alt="About our programme - First World War 100">
 										</div>
 											<h3>First World War 100</h3>
 											<p>We are marking the centenary of the war with an extensive programme, spanning a five-year period between 2014 and 2019.</p>
@@ -229,7 +229,7 @@ get_header(); ?>
 								<div class="entry-content clearfix">
 									<div class="fww-box clearfix">
 										<div class="thumb-img">
-											<img src="<?php echo get_stylesheet_directory_uri() ?>/img/whatson-CAB-45_7_1914.jpg" class="img-responsive" alt="First World War 100 events calendar">
+											<img src="<?php echo make_path_relative( get_stylesheet_directory_uri() ) ?>/img/whatson-CAB-45_7_1914.jpg" class="img-responsive" alt="First World War 100 events calendar">
 										</div>
 										<h3>Events calendar</h3>
 										<div id="event-list"></div>
@@ -253,7 +253,7 @@ get_header(); ?>
 								<div class="entry-content clearfix">
 									<div class="fww-box clearfix">
 										<div class="thumb-img">
-												<img src="<?php echo get_stylesheet_directory_uri() ?>/img/tnabookshop.jpg" class="img-responsive" alt="Visit The National Archives Bookshop at Kew">
+												<img src="<?php echo make_path_relative( get_stylesheet_directory_uri() ) ?>/img/tnabookshop.jpg" class="img-responsive" alt="Visit The National Archives Bookshop at Kew">
 										</div>
 											<h3>Opening hours</h3>
 											<p>Visit The National Archives Bookshop at Kew.<br>Open 09:00-17:00 Tuesday to Saturday.</p>
