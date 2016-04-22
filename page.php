454fe215c3279d0e1305330a83372dc3fea22746
Chris Bishop <domingobishop@gmail.com>
2016-04-22 14:29:55 +0100

    added banner image and amended js for pages

--- page.php
+++ page.php
@@ -13,7 +13,7 @@
 	<div id="primary" class="content-area">
 		<div class="container">
 			<div class="row">
-				<main id="main" class="col-xs-12 col-sm-8" role="main">
+				<main id="main" class="col-xs-12 col-md-8" role="main">
 					<?php
 					while ( have_posts() ) : the_post();
 						get_template_part( 'content' );
