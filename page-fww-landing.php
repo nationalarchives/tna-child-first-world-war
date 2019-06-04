fd14d7f6ddb2e53475ed1da798c69b2259918800
AshTNA <ashleyaburnett@gmail.com>
2019-06-04 11:23:50 +0100

    Fix RSS feed url as old url 404'd

--- page-fww-landing.php
+++ page-fww-landing.php
@@ -123,7 +123,7 @@ get_header(); ?>
 						<div class="equal-heights equal-heights-flex-box">
 
 						<!-- Dynamically pulls the latest blog item with the tagged first-world-war -->
-						<?php make_path_relative( fww_rss( 'https://blog.nationalarchives.gov.uk/blog/tag/first-world-war/feed/', '1' ) ) ?>
+						<?php make_path_relative( fww_rss( 'https://blog.nationalarchives.gov.uk/tag/first-world-war/feed/', '1' ) ) ?>
 
 						<!-- Dynamically pulls the latest news item with the category first-world-war-portal-news -->
 						<?php make_path_relative( fww_news_rss( 'https://www.nationalarchives.gov.uk/category/first-world-war-portal-news/feed/', '2' ) ) ?>
